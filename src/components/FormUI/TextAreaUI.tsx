cfa03e9cb915777b35a5523ad1ee8d492674557c
sami_dev <samanahrari@yahoo.com>
2023-07-29 10:41:55 +0330

    BUGFIXED: fixed a bug in displaying error border in text area ui

--- src/components/FormUI/TextAreaUI.tsx
+++ src/components/FormUI/TextAreaUI.tsx
@@ -15,9 +15,10 @@ const TextAreaUI = (
   }: TextAreaUIProps,
   ref: Ref<HTMLTextAreaElement>
 ) => {
+  console.log(borderVariant)
   return (
     <textarea
-      className={`p-16 outline-none bg-transparent transition duration-200 rounded-8 border-gray-7 font-body-sm border-2 border-solid text-gray-1 placeholder:text-gray-1 ${className} ${className} ${borderVariants[borderVariant]}`}
+      className={`p-16 outline-none bg-transparent transition duration-200 rounded-8 font-body-sm text-gray-1 placeholder:text-gray-1 ${className} ${className} ${borderVariants[borderVariant]}`}
       placeholder={placeholder}
       style={{
         resize: resize,
