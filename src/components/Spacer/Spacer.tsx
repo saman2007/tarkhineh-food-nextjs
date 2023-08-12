bf2776143232e7c51409330fb511134d6d7d858d
sami_dev <samanahrari@yahoo.com>
2023-08-13 01:41:32 +0330

    MODIFIED & FEATURE: added document for Spacer component and removed children prop from Spacer component.

--- src/components/Spacer/Spacer.tsx
+++ src/components/Spacer/Spacer.tsx
@@ -1,10 +1,11 @@
 interface Props {
+  /** height of Spacer in px */
   height: number;
-  children?: React.ReactNode;
 }
 
-const Spacer = ({ children, height }: Props) => {
-  return <div style={{ height: height + "px" }}>{children}</div>;
+/** a spacer component is used as a block element with a specific height to compensate for the height of elements with a fixed position */
+const Spacer = ({ height }: Props) => {
+  return <div style={{ height: height + "px" }}></div>;
 };
 
 export default Spacer;
