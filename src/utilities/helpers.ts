6394b927cd62c12f61030428cfd51875b589050a
sami_dev <samanahrari@yahoo.com>
2023-08-18 23:28:28 +0330

    FEATURE: added doc for helper functions

--- src/utilities/helpers.ts
+++ src/utilities/helpers.ts
@@ -1,6 +1,6 @@
 /**
- * a helper function to return new refrence from an object
- * @param obj obj is a the object you want to deep clone
+ * A helper function to return new refrence from an object
+ * @param obj obj is the object you want to deep clone
  * @returns a deep cloned object
  */
 export const cloneDeep = <T>(obj: T): T => JSON.parse(JSON.stringify(obj));
