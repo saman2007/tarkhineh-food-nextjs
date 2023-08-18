3c3e78b2dbd2407e2a0144a41f34fa828b2ef5c7
sami_dev <samanahrari@yahoo.com>
2023-08-18 13:52:56 +0330

    FEATURE & MODIFIED: added doc for Portal component and modified the way of understanding that the component is rendred or not, using a new hook called useIsMounted.

--- src/components/Portal/Portal.tsx
+++ src/components/Portal/Portal.tsx
@@ -1,19 +1,19 @@
 "use client";
 
+import useIsMounted from "@/hooks/useIsMounted";
 import { useEffect, useState } from "react";
 import { createPortal } from "react-dom";
 
 interface Props {
+  /** elements you want to create portal for */
   children: React.ReactNode;
+  /** the id of element that is the portal */
   portalId: string;
 }
 
+/** a component to use create portal for your elements easily */
 const Portal = ({ children, portalId }: Props) => {
-  const [isMounted, setIsMounted] = useState(false);
-
-  useEffect(() => {
-    setIsMounted(true);
-  }, []);
+  const isMounted = useIsMounted();
 
   const el =
     typeof document !== "undefined" && document.getElementById(portalId);
